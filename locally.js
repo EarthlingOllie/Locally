4ab17f0069de266ff10e0757249dfdd1e6f67fed
ONDoubleB <oliverbullen@me.com>
2014-05-21 21:06:34 +0100

    configuring getters and setters

--- locally.js
+++ locally.js
@@ -1,60 +1,81 @@
 //Locally, a simple yet efficient way to store local data efficiently.
 
-/*
-
- Locally.prototype._init = function() {
-
- if(typeof Storage !== 'undefined') {
- if(typeof localStorage !== 'undefined') {
-
- } else {
- log('This browser does not support local storage');
- }
-
- if(typeof sessionStorage !== 'undefined') {
-
- } else {
- log('This browser does not support session storage');
- }
- } else {
- log('This browser does not support any storage');
- }
- };
-
- Locally._init();
-
-
- */
-
 var Locally = {
 
-    /* Session data will be stored in here */
-    _session: [],
-    /* Persistent storage will be stored in here */
-    _persistent: [],
+	//Holds a reference to window.localStorage
+	_localStorage: {},
 
-    init: function() {
+	//Used for accessing the _localStorage property
+	set localStorage(value) {
+		this._localStorage = value;
+	},
+	get localStorage() {
+		return this._localStorage;
+	},
 
-        if(typeof Storage !== 'undefined') {
+	//Holds a reference to window.sessionStorage
+	_sessionStorage: {},
 
-        } else {
-            console.info('This browser does not support Locally');
-        }
-    },
+	//Used for accessing the _sessionStorage property
+	set sessionStorage(value) {
+		this._sessionStorage = value;
+	},
+	get sessionStorage() {
+		return this._sessionStorage;
+	},
 
-    session: {
-        init: function() {
-            if(!this._session[name]) {
-                localStorage.setItem(name, []);
-            }
-        },
-        find: function(query) {
-            console.log(query);
-        }
-    }
+	//A representation of all data in localStorage
+	_localData: [],
+
+	//Used for accessing the _localData property
+	set localData(obj) {
+		if(obj.index && obj.value) {
+			if(this._localData[obj.index] && !this._localData[obj.index].protected) {
+				this._localData[obj.index] = obj.value;
+			} else {
+				throw 'localData at index ' + obj.index + 'is protected and cannot be modified.';
+			}
+		} else {
+			throw 'localData expects an index at Obj.index and value at Obj.value';
+		}
+	},
+	get localData() {
+		return this._localData;
+	},
+
+	//A representation of all data in sessionStorage
+	_sessionData: [],
+
+	//Used for accessing the _sessionData property
+	set sessionData(obj) {
+		if(obj.index && obj.value) {
+			if(this._sessionData[obj.index] && !this._sessionData[obj.index].protected) {
+				this._sessionData[obj.index] = obj.value;
+			} else {
+				throw 'sessionData at index ' + obj.index + 'is protected and cannot be modified.';
+			}
+		} else {
+			throw 'sessionData expects an index at Obj.index and value at Obj.value';
+		}    },
+	get sessionData() {
+		return this._sessionData;
+	},
+
+
+	//Initialise the Locally Object
+	init: function() {
+
+		if(typeof Storage !== 'undefined') {
+
+			this.localStorage = localStorage;
+			this.sessionStorage = sessionStorage;
+		} else {
+			console.info('Locally isn\'t supported on this browser.');
+		}
+	}
 };
 
 
-Locally.session('test');
-
-console.log('hello');
+Locally.init();
+Locally.localStorage.setItem('Hello', 'There');
+console.log(localStorage.getItem('Hello'));
